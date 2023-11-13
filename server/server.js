89201b1de273c1081695903cc879f14a52eb6d13
ebarroso24 <134114010+ebarroso24@users.noreply.github.com>
2023-11-13 13:45:11 -0500

    added schemas folder and files to server folder

--- server/server.js
+++ server/server.js
@@ -1,5 +1,29 @@
+// const express = require('express');
+// const path = require('path');
+// const db = require('./config/connection');
+// const routes = require('./routes');
+
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// // if we're in production, serve client/build as static assets
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
+
+// app.use(routes);
+
+// db.once('open', () => {
+//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// });
+
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const routes = require('./routes');
 
@@ -9,13 +33,28 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+// Connect to MongoDB
+db.once('open', () => {
+  console.log('Connected to the database');
+});
+
+// Apollo Server setup
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.applyMiddleware({ app });
+
+// Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+app.listen(PORT, () => {
+  console.log(`🌍 Now listening on localhost:${PORT}`);
+  console.log(`GraphQL Playground: http://localhost:${PORT}${server.graphqlPath}`);
 });
+
